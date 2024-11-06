b499c418ac4dec490a9eb71994f98ad869c3094f
OdurS <odur.schweikert@gmail.com>
2024-11-06 20:48:47 +0000

    Übunsblatt1 final

--- Aufgabe 1/Übungsblatt1/Aufgabe2.py
+++ Aufgabe 1/Übungsblatt1/Aufgabe2.py
@@ -2,19 +2,20 @@ import math
 size=int(input("Wie groß soll das Haus sein?"))#größe des Hauses
 i=size
 breite=size*2
-d=size    
+d=size 
+bs = "\\"   
 while i>1:
     #print(" "*(i-1) + "/" + " "*(2*(size-i)) + "\\")
-    s = "/"^^
-    bs = "\\"
+    
     print(f"{'/':>{i}}{bs:>{(2*(size - i) + 1)}}")
     i=i-1
-#print("/" + "_"*(breite-2) + "\\")
-print(f"{'/':{1}}{'_':>{(breite-2)}}{bs:{1}}")
+print("/" + "_"*(breite-2) + "\\")
+
+#print(f"{'/':{1}}{'_':>{(breite-2)}}{bs:{1}}")
 a=1
 while a<=size-1:
     print("|" + " "*(breite-2) + "|")
     a=a+1
-#print("|" +  "_"*(breite-2) + "|")
-print(f"{'|':{1}}{'_':>{(breite-2)}}{|:{1}}")
+print("|" +  "_"*(breite-2) + "|")
+#print(f"{'|':{1}}{'_':>{(breite-2)}}{'|':{1}}")
 
